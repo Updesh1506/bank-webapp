887a5efd0ed8489849a825ffd25900a1f7e944c4
shashwat6797 <shashlko2002@gmail.com>
2024-12-01 19:27:14 +0530

    FIX: added bank card with static data

--- src/routes/myBanks.jsx
+++ src/routes/myBanks.jsx
@@ -1,27 +1,32 @@
-import BankCard from "@/components/BankCard"
-import HeaderBox from "@/components/HeaderBox"
-
+import BankCard from "@/components/BankCard";
+import HeaderBox from "@/components/HeaderBox";
 
 const MyBanks = () => {
+  const account = {
+    name: "Shashwat",
+    id: 1,
+    currentBalance: 142490,
+  };
+
+  const userName = "shashwat";
+
+  const showBalance = true;
   return (
-   <section className="flex">
-    <div className="my-banks">
-      <HeaderBox title="My Banks Accounts" subtext="Effortlessly manage your banking activities."/>
-      <div className="space-y-4">
-        <h2 className="header-2">
-          Your cards
-
-        </h2>
-        <div className="flex flex-wrap gap-6">
-           BankCards which are on home page will be shown here
+    <section className="flex">
+      <div className="my-banks">
+        <HeaderBox
+          title="My Banks Accounts"
+          subtext="Effortlessly manage your banking activities."
+        />
+        <div className="space-y-4">
+          <h2 className="header-2">Your cards</h2>
+          <div className="flex flex-wrap gap-6">
+            <BankCard account={account} userName={userName} showBalance={showBalance}/>
+          </div>
         </div>
-
       </div>
+    </section>
+  );
+};
 
-    </div>
-
-   </section>
-  )
-}
-
-export default MyBanks
+export default MyBanks;
